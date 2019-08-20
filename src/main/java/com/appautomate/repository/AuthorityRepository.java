c1821df4f8b3cc7c81787cc28a5d91648150669b
Jemal <jemal@zoomdata.com>
2019-08-20 10:10:24 -0400

    Initial application generated by JHipster-5.8.0

---
+++ src/main/java/com/appautomate/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.appautomate.repository;
+
+import com.appautomate.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
